75c5a4bec3558c32c3f50f5de4e2aa45a6fd600e
lubasi <lubasi@example.com>
2026-06-04 19:43:22 +0200

    Remove nvim files to prepare for submodule

--- nvim/.config/nvim/lua/config/plugins/treesitter.lua
+++
@@ -1,9 +0,0 @@
-return {
-    'nvim-treesitter/nvim-treesitter',
-    build = ':TSUpdate',
-    branch = 'main',
-    event = 'VeryLazy',
-    config = function()
-        require("nvim-treesitter").setup({})
-    end
-}
